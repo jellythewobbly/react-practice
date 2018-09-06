f3444fff0688c3434722b4489610c28c68b6c47e
jellythewobbly <jellythewobbly@hotmail.com>
2018-09-07 04:59:07 +0800

    Added navbar, working on router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from './components/movies';
+import NavBar from './components/common/navBar';
 
 class App extends Component {
 	render() {
 		return (
-			<main className="container">
-				<Movies />
-			</main>
+			<React.Fragment>
+				<NavBar />
+				<main className="container">
+					<Movies />
+				</main>
+			</React.Fragment>
 		);
 	}
 }
