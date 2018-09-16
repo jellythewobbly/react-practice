a4fc3b008b21b3d0b624445d744be348ac9887b1
jellythewobbly <jellythewobbly@hotmail.com>
2018-09-16 22:49:18 +0800

    added register form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 import './App.css';
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
 						<Route path="/movies" component={Movies} />
 						<Route path="/customers" component={Customers} />
 						<Route path="/rentals" component={Rentals} />
+						<Route path="/register" component={RegisterForm} />
 						<Route path="/not-found" component={NotFound} />
 						<Redirect from="/" exact to="movies" />
 						<Redirect to="/not-found" />
